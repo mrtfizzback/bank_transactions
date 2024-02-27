2e18ed3c4a0fb463fc9e502b110b9bb73ee5beed
jose_Almeida <josepedrosmith@msn.com>
2024-02-27 19:10:00 +0000

    first commit

---
+++ src/test/java/com/josealmeida/banktransactions/BankTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josealmeida.banktransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
