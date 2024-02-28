38dae5d3d0f3804734b1a16dff96e7ee86a70a7b
jose_Almeida <josepedrosmith@msn.com>
2024-02-28 16:25:50 +0000

    Tests for modify transaction are working

--- src/test/java/com/josealmeida/banktransactions/serviceTest/TransactionServiceTest.java
+++ src/test/java/com/josealmeida/banktransactions/serviceTest/TransactionServiceTest.java
@@ -54,5 +54,58 @@ public class TransactionServiceTest {
         verify(transactionRepository, times(1)).save(any());
     }
 
+    @Test
+    public void testModifyTransaction_Success() {
+        // Arrange
+        Long id = 1L;
+        TransactionRequest request = TransactionRequest.builder()
+                .schedulingDate(LocalDate.now().plusDays(2))
+                .amount(BigDecimal.valueOf(1500)) // Entre 1001 e 2000 para Taxa B
+                .build();
+        Transaction existingTransaction = Transaction.builder()
+                .id(id)
+                .schedulingDate(LocalDate.now().plusDays(1))
+                .amount(BigDecimal.valueOf(1200))
+                .build();
+        Transaction updatedTransaction = Transaction.builder()
+                .id(id)
+                .schedulingDate(request.getSchedulingDate())
+                .amount(request.getAmount())
+                .build();
+
+        when(transactionRepository.findById(id)).thenReturn(Optional.of(existingTransaction));
+        when(transactionRepository.save(any())).thenReturn(updatedTransaction);
+
+        // Act
+        Transaction modifiedTransaction = transactionService.modifyTransaction(id, request);
+
+        // Assert
+        assertEquals(updatedTransaction.getSchedulingDate(), modifiedTransaction.getSchedulingDate());
+        assertEquals(updatedTransaction.getAmount(), modifiedTransaction.getAmount());
+        verify(transactionRepository, times(1)).findById(id);
+
+    }
+
+    @Test
+    public void testModifyTransaction_NotFound() {
+        // Arrange
+        Long id = 999L; // Id de Transação que não existe
+        TransactionRequest request = TransactionRequest.builder()
+                .schedulingDate(LocalDate.now().plusDays(2))
+                .amount(BigDecimal.valueOf(1500))
+                .build();
+
+        when(transactionRepository.findById(id)).thenReturn(Optional.empty());
+
+        // Act + Assert
+        try {
+            transactionService.modifyTransaction(id, request);
+        } catch (RuntimeException e) {
+            assertEquals("Transaction with id " + id + " not found", e.getMessage());
+        }
+
+        // Assert
+        verify(transactionRepository, times(1)).findById(id);
+    }
 
 }
